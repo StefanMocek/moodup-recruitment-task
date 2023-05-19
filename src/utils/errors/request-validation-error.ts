b2b76bdbc3aa6362699abe9e501ad2ab3f5a3265
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-05-19 19:14:32 +0200

    errors handling fixed

--- src/utils/errors/request-validation-error.ts
+++ src/utils/errors/request-validation-error.ts
@@ -1,16 +1,18 @@
-import {ValidationError} from 'express-validator'
-import {CustomError} from './custom-error'
+import {ValidationError} from 'express-validator';
+import {CustomError} from './custom-error';
 
 export class RequestValidationError extends CustomError {
     statusCode = 400;
 
     constructor(public errors: ValidationError[]) {
-        super('Invalid request')
+        super('Invalid request parameters');
+
+        Object.setPrototypeOf(this, RequestValidationError.prototype);
     }
 
-    generateErrors() {
-        return this.errors.map(error => {
-            return {message: error.msg}
-        })
+    serializeErrors() {
+        return this.errors.map(err => {
+            return {message: err.msg};
+        });
     }
 }
