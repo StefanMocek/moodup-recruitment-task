04161549a3bbe7e0015a7836f923d8cf91f01bbd
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-05-19 15:33:36 +0200

    recipe model

---
+++ src/recipes/recipe/recipe.model.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+import {UserDoc} from '../../auth/user/user.model';
+
+interface RecipeDoc extends mongoose.Document {
+  user: UserDoc | string,
+  name: string,
+  ingredients: { [key: string]: string}[],
+  preparing: { [key: number]: string}[],
+  time: string;
+  image?: string
+}
+
+interface RecipeModel extends mongoose.Model<RecipeDoc> {}
+
+const schema = new mongoose.Schema<RecipeDoc, RecipeModel>({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [
+      {
+        key: {
+          type: String,
+          required: true,
+        },
+        value: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    required: true,
+  },
+  preparing: {
+    type: [
+      {
+        key: {
+          type: Number,
+          required: true,
+        },
+        value: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+});
+
+export const Recipe = mongoose.model<RecipeDoc, RecipeModel>('Recipe', schema);
