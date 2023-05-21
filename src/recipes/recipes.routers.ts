e9d36270cd90e8b24a6c64f0a0e33a6ca40822f7
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-05-21 18:22:11 +0200

    app routes changed

--- src/recipes/recipes.routers.ts
+++ src/recipes/recipes.routers.ts
@@ -6,11 +6,11 @@ import {recipeValidation} from './validatiors/add-recipe';
 
 const router = Router();
 
-router.get('/recipes', requireAuth, RecipesController.getAllRecipes);
-router.post('/recipes/add', recipeValidation, validateRequest, requireAuth, RecipesController.addRecipe);
-router.post('/recipes/update/:id', recipeValidation, validateRequest, requireAuth, RecipesController.updateRecipe)
-router.delete('/recipes/delete/:id', requireAuth, RecipesController.deleteRecipe);
-router.post('/recipes/add-image-to-recipe/:id', upload.single('image'), RecipesController.sendImageToS3)
-router.get('/recipes/:id', requireAuth, RecipesController.getSingleRecipe);
+router.get('/', requireAuth, RecipesController.getAllRecipes);
+router.post('/add', recipeValidation, validateRequest, requireAuth, RecipesController.addRecipe);
+router.post('/update/:id', recipeValidation, validateRequest, requireAuth, RecipesController.updateRecipe)
+router.delete('/delete/:id', requireAuth, RecipesController.deleteRecipe);
+router.post('/add-image-to-recipe/:id', upload.single('image'), RecipesController.sendImageToS3)
+router.get('/:id', requireAuth, RecipesController.getSingleRecipe);
 
 export {router as recipesRouters};
