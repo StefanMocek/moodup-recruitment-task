f8ee944a63ce6bbe754345ac3c5c711439f06111
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-05-21 19:05:40 +0200

    recipe routes improved

--- src/recipes/recipes.routers.ts
+++ src/recipes/recipes.routers.ts
@@ -6,11 +6,19 @@ import {recipeValidation, idValidation} from './validatiors';
 
 const router = Router();
 
-router.get('/', requireAuth, RecipesController.getAllRecipes);
-router.post('/add', recipeValidation, validateRequest, requireAuth, RecipesController.addRecipe);
-router.post('/update/:id', recipeValidation, idValidation, validateRequest, requireAuth, RecipesController.updateRecipe)
-router.delete('/delete/:id', idValidation, validateRequest, requireAuth, RecipesController.deleteRecipe);
-router.post('/add-image-to-recipe/:id', idValidation, validateRequest, upload.single('image'), RecipesController.sendImageToS3)
-router.get('/:id', idValidation, validateRequest, requireAuth, RecipesController.getSingleRecipe);
+router
+  .route('/')
+  .post(recipeValidation, validateRequest, requireAuth, RecipesController.addRecipe)
+  .get(requireAuth, RecipesController.getAllRecipes);
+
+router
+  .route('/add-image-to-recipe/:id')
+  .post(idValidation, validateRequest, upload.single('image'), RecipesController.sendImageToS3)
+
+router
+  .route('/:id')
+  .post(recipeValidation, idValidation, validateRequest, requireAuth, RecipesController.updateRecipe)
+  .delete(idValidation, validateRequest, requireAuth, RecipesController.deleteRecipe)
+  .get(idValidation, validateRequest, requireAuth, RecipesController.getSingleRecipe);
 
 export {router as recipesRouters};
